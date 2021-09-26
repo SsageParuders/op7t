0ac6eab07f948d27898f6603de00cc461d9e9384
buutuud <buutuud@gmail.com>
2021-09-26 17:39:53 +0800

    fix anti-anti-debug

--- blu7t/op7-r70/fs/proc/base.c
+++ blu7t/op7-r70/fs/proc/base.c
@@ -362,18 +362,18 @@ static int proc_pid_wchan(struct seq_file *m, struct pid_namespace *ns,
 
 	return 0;
 	*/
-	if (lookup_symbol_name(wchan, symname) < 0) {
-        if (!ptrace_may_access(task, PTRACE_MODE_READ))
-            return 0;
-        else
-            return sprintf(buffer, "%lu", wchan);
+	if (wchan && ptrace_may_access(task, PTRACE_MODE_READ_FSCREDS)
+			&& !lookup_symbol_name(wchan, symname)) {
+		if (strstr(symname, "trace")) {
+            seq_printf(m, "%s", "sys_epoll_wait");
+        } else {
+			seq_printf(m, "%s", symname);
+		}
 	}
-    else {
-        if (strstr(symname, "trace")) {
-            return sprintf(buffer, "%s", "sys_epoll_wait");
-        }
-        return sprintf(buffer, "%s", symname);
-    }
+	else
+		seq_putc(m, '0');
+
+	return 0;
 }
 #endif /* CONFIG_KALLSYMS */
 
